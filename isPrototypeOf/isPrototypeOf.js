8d9b4d07f204f1d3d3f838858a81d51f4b546a8e
justinbourb <30423126+justinbourb@users.noreply.github.com>
2018-11-12 22:41:00 -0500

    added functionality when the prototype has more than one key

--- isPrototypeOf/isPrototypeOf.js
+++ isPrototypeOf/isPrototypeOf.js
@@ -1,5 +1,14 @@
+
 function isPrototypeOf(proto, obj){
 	try{
+	//checkArrayOfKeys allows us to loop of an array of keys if the prototype has multiple keys
+      	function checkArrayOfKeys(objToCheck, arr) {
+            for(var i = 0; i < arr.length; i++) {
+                if(typeof(objToCheck[arr[i]]) == "function")
+                    return true;
+            }
+            return false;
+        }
         // make sure the prototype of obj is found on proto   &&    make sure proto and obj do not have the same keys (which would imply they are both prototypes of something else)
         // second bit of logic does not work for higher order prototypes (that go up more than one level)
         if (typeof(proto[Object.keys(Object.getPrototypeOf(obj))]) !== 'undefined' && Object.keys(proto).toString() !== Object.keys(obj).toString()){
@@ -8,8 +17,9 @@ function isPrototypeOf(proto, obj){
 			      return true
         }
         //check for higher level prototypes: the key of higher level prototype will be callable as a prototype on the obj
-        if (typeof(obj[Object.keys(proto).toString()])==="function"){
-           return true 
+        if (Object.keys(proto).length > 0){
+          
+           return checkArrayOfKeys(obj, Object.keys(proto)) 
         }else{
             return false
         }
