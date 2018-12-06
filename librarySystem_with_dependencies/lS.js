d28268c456742f2a8e431c70c1111e675719bd06
justinbourb <30423126+justinbourb@users.noreply.github.com>
2018-12-06 15:53:08 -0500

    airbnb style guide

--- librarySystem_with_dependencies/lS.js
+++ librarySystem_with_dependencies/lS.js
@@ -34,109 +34,89 @@
 
 
 (function () {
-  
-  let store = {};
-  let dependenciesNotLoaded = [];
-  let updated = [];     
-  function checkDependency(array) {
-   
-   //this function will recursively check if a library's dependencies are loaded
-    array.forEach(function(element) {
-      if (!store[element]) {
-        dependenciesNotLoaded.push(element);
-      }
-      try {
-          if(store[element].dependency.length > 0){
-              return checkDependency(store[element].dependency)
-          }
-      } catch {}
-      });
+	let store = {};
+	let dependenciesNotLoaded = [];
+	let updated = [];     
+	function checkDependency(array) {
+	 //this function will recursively check if a library's dependencies are loaded
+	 array.forEach(function(element) {
+		if (!store[element]) {
+		 dependenciesNotLoaded.push(element);
+		}
+		try {
+		 if(store[element].dependency.length > 0){
+			return checkDependency(store[element].dependency)
+			}
+		} catch {}
+	 });
 
   }
        
   //lS is short for librarySystem
   function lS(name, dependency, callback) {
-   
-    //creating a library 
-   if (arguments.length > 1) {
-    
-      //Case 1: dependency is given as an object
+    //Case 1: creating a library 
+    if (arguments.length > 1) {
+      //Case 1a: dependency is given as an object
       if (typeof(dependency) === "object" && typeof(callback) === "function") {
-       
-        //case 1a: dependency is provided
+        //case 1a1: dependency is provided
         if (dependency.length > 0) {
-          store[name] = {name: name, dependency: dependency, callback: callback, 
-                       callback_results: undefined};
-          return
-          
-        //case 1b: lS is called with an empty array or string for dependency
+        store[name] = {name: name, dependency: dependency, callback: callback, 
+                  callback_results: undefined};
+        return
+        //case 1a2: lS is called with an empty array or string for dependency
         } else {
-          store[name] = {name: name, dependency: dependency, callback: callback, 
-                       callback_results: callback()};
-          return
+        store[name] = {name: name, dependency: dependency, callback: callback, 
+                  callback_results: callback()};
+        return
         }
-          
       }
-      //Case 2: dependency is given as an string
-    
+      //Case 1b: dependency is given as an string
       if (typeof(dependency) === "string" && typeof(callback) === "function") {
-       
-        //case 2a: dependency is provided
-        if (dependency.length > 0) {
-          store[name] = {name: name, dependency: dependency.split(), callback: callback, 
-                       callback_results: undefined};
-          return
-          
-        //case 2b: lS is called with an empty array or string for dependency
-        } else {
-          store[name] = {name: name, dependency: dependency, callback: callback, 
-                       callback_results: callback()};
-          return
-        }
-          
+         //case 1b1: dependency is provided
+         if (dependency.length > 0) {
+           store[name] = {name: name, dependency: dependency.split(), callback: callback, 
+                        callback_results: undefined};
+           return
+         //case 1b2: lS is called with an empty array or string for dependency
+         } else {
+           store[name] = {name: name, dependency: dependency, callback: callback, 
+                        callback_results: callback()};
+           return
+         }
       }
-    
-       //Case 3: dependency is not provided, therefore the dependency variable is the function
-       if (typeof(dependency) === "function" && callback === undefined) {
-         store[name] = {name: name, dependency: undefined, callback: dependency, 
-                       callback_results: dependency()};
-         return
-         
-       } else {
-         return `Error: Improper format used.  
-        To store: lS('name'[,dependency], function);
-        To call : lS('name');`
-              
-       }
-    
-   } else {
-    
-     //return a library
-     //check if the callback function was run / stored
-     if (store[name].callback_results !== undefined) {
-       return store[name].callback_results
-      
-     } else {
-      checkDependency(store[name].dependency);
-      
-      //case 1: there are dependencies which haven't been loaded, notify the user
-      if (dependenciesNotLoaded.length > 0){
-       return "Dependencies not loaded: " + dependenciesNotLoaded.toString().replace(',', ', ')
-       
-      //case 2: all dependencies are loaded
+      //Case 1c: dependency is not provided, therefore the dependency variable is the function
+      if (typeof(dependency) === "function" && callback === undefined) {
+        store[name] = {name: name, dependency: undefined, callback: dependency, 
+                     callback_results: dependency()};
+        return
       } else {
-       
-        //prepare dependency array for apply.() usage
-        store[name].dependency.forEach(function(element) {
-          updated.push(store[element].callback_results);
-        });
-       
-        //store callback results
-        store[name].callback_results = store[name].callback.apply(null, updated);
-        return store[name].callback_results
+        return `Error: Improper format used.  
+                To store: lS('name'[,dependency], function);
+                To call : lS('name');`
       }
-     }
-   }
+    //Case 2: return a library
+    } else {
+      //Case 2a: check if the callback function was run / stored and return results if already run
+      if (store[name].callback_results !== undefined) {
+       return store[name].callback_results
+      } else {
+        //check for any missing dependencies
+        checkDependency(store[name].dependency);
+        //Case 2b: there are dependencies which haven't been loaded, notify the user
+        if (dependenciesNotLoaded.length > 0){
+         return "Dependencies not loaded: " + dependenciesNotLoaded.toString().replace(',', ', ')
+        //Case 2c: all dependencies are loaded
+        } else {
+          //prepare dependency array for apply.() usage
+          store[name].dependency.forEach(function(element) {
+            updated.push(store[element].callback_results);
+          });
+          //store callback results
+          store[name].callback_results = store[name].callback.apply(null, updated);
+          return store[name].callback_results
+        }
+      }
+    }
   }
   window.lS = lS;
 })();
